f908e0df0edba00a54f072c0bb2e01d4b4537c03
weiqidong <1139755334@qq.com>
2019-07-30 20:48:30 +0800

    xj

--- house/src/views/booking.vue
+++ house/src/views/booking.vue
@@ -2,8 +2,8 @@
   <div>
     <div>
       <div>
-        <h3>订单信息</h3>
-        <hr>
+        <div>订单信息</div>
+        <div>
         入住时间段：<el-date-picker
       v-model="value7"
       type="daterange"
@@ -13,7 +13,8 @@
       start-placeholder="开始日期"
       end-placeholder="结束日期"
       :picker-options="pickerOptions2">
-    </el-date-picker>
+        </el-date-picker>
+        </div>
       </div>
     </div>
   </div>
@@ -22,7 +23,34 @@
 export default {
   data(){
     return {
-
+      pickerOptions2: {
+          shortcuts: [{
+            text: '最近一周',
+            onClick(picker) {
+              const end = new Date();
+              const start = new Date();
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
+              picker.$emit('pick', [start, end]);
+            }
+          }, {
+            text: '最近一个月',
+            onClick(picker) {
+              const end = new Date();
+              const start = new Date();
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
+              picker.$emit('pick', [start, end]);
+            }
+          }, {
+            text: '最近三个月',
+            onClick(picker) {
+              const end = new Date();
+              const start = new Date();
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
+              picker.$emit('pick', [start, end]);
+            }
+          }]
+        },
+        value7: [new Date(sessionStorage.getItem("value3")),new Date(sessionStorage.getItem("value7"))],
     }
   }
 }
